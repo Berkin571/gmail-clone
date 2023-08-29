d086623f75ba64370e6968c34f12432d51cdaa1f
bilgin <Berkin.Bilgin@adesso.de>
2023-08-29 09:08:37 +0200

    feat: sidebar

--- src/components/sidebar/sidebar-option/sidebar-option.component.tsx
+++ src/components/sidebar/sidebar-option/sidebar-option.component.tsx
@@ -1,19 +1,23 @@
+import { useState } from "react";
 import "./sidebar-option.scss";
 
 type SidebarOptionProps = {
-  Icon: any;
+  Icon: React.ElementType;
   title: string;
   number: number;
 };
 
 export function SidebarOption({ Icon, title, number }: SidebarOptionProps) {
+  const [selected, setSelected] = useState<boolean>(false);
+
   return (
-    <div className="sidebarOption">
+    <div
+      className={`sidebarOption ${selected && "sidebarOption--active"}`}
+      onClick={() => setSelected(!selected)}
+    >
       <Icon />
       <h3>{title}</h3>
       <p>{number}</p>
     </div>
   );
 }
-
-// 3:47 std
--- src/components/sidebar/sidebar.component.tsx
+++ src/components/sidebar/sidebar.component.tsx
@@ -1,8 +1,17 @@
 import "./sidebar.scss";
 
-import { Button } from "@mui/material";
+import { Button, IconButton } from "@mui/material";
 import AddIcon from "@mui/icons-material/Add";
 import InboxIcon from "@mui/icons-material/Inbox";
+import StarIcon from "@mui/icons-material/Star";
+import AccessTimeIcon from "@mui/icons-material/AccessTime";
+import LabelImportantIcon from "@mui/icons-material/LabelImportant";
+import NearMeIcon from "@mui/icons-material/NearMe";
+import NoteIcon from "@mui/icons-material/Note";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import PersonIcon from "@mui/icons-material/Person";
+import DuoIcon from "@mui/icons-material/Duo";
+import PhoneIcon from "@mui/icons-material/Phone";
 import { SidebarOption } from "./sidebar-option/sidebar-option.component";
 
 export function Sidebar() {
@@ -16,6 +25,26 @@ export function Sidebar() {
       </Button>
 
       <SidebarOption Icon={InboxIcon} title="Eingang" number={54} />
+      <SidebarOption Icon={StarIcon} title="Favoriten" number={54} />
+      <SidebarOption Icon={AccessTimeIcon} title="Archiv" number={54} />
+      <SidebarOption Icon={LabelImportantIcon} title="Wichtig" number={54} />
+      <SidebarOption Icon={NearMeIcon} title="Gesendet" number={54} />
+      <SidebarOption Icon={NoteIcon} title="Entwürfe" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
+
+      <div className="sidebar__footer">
+        <div className="sidebar__footerIcons">
+          <IconButton>
+            <PersonIcon />
+          </IconButton>
+          <IconButton>
+            <DuoIcon />
+          </IconButton>
+          <IconButton>
+            <PhoneIcon />
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 }
